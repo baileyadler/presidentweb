eaf7a50e6c5fa46356ff41c240793c9d28c67d22
baileyadler <526BAdler@frhsd.com>
2025-03-03 10:40:34 -0500

    need to add more stuff

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
-var quotesArray = [
-    'Roosevelt',
-    'ok',
-    'teddy'
-];
-var randomNumber = Math.floor(Math.random() * quotesArray.length);
-document.getElementById('press-quote').textContent = '“' + quotesArray[randomNumber].split(' — ')[0] + '” —' + quotesArray[randomNumber].split(' —')[1] ;
 
 
